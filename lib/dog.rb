35aa2af07c0b903d18257697b9cd336ed7b61571
Aaron Lieberman <aaronmaxmusic@gmail.com>
2018-03-29 19:39:37 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -7,4 +7,22 @@ attr_accessor :name, :breed, :id
     @breed = breed
     @id = id
   end
+
+  def self.create_table
+  sql = <<-SQL
+    CREATE TABLE IF NOT EXISTS dogs (
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      breed TEXT
+    )
+    SQL
+    DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    sql = <<-SQL
+      DROP TABLE dogs
+    SQL
+    DB[:conn].execute(sql)
+  end
 end
