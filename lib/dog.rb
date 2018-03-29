e2a9da8f1853267fe67377c6a48d524830d458ea
Aaron Lieberman <aaronmaxmusic@gmail.com>
2018-03-29 19:42:36 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -25,4 +25,21 @@ attr_accessor :name, :breed, :id
     SQL
     DB[:conn].execute(sql)
   end
+
+  def self.new_from_db(row)
+    id = row[0]
+    name = row[1]
+    grade = row[2]
+    self.new(id, name, breed)
+  end
+
+  def save
+    sql = <<-SQL
+      INSERT INTO dogs (name, breed)
+      VALUES (?, ?)
+    SQL
+
+    DB[:conn].execute(sql, self.name, self.breed)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+  end
 end
